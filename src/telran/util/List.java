4f35b4d9fe47b3fe42cd45cc5fa22ca75dd2c985
Mancore <fairgvard@gmail.com>
2023-07-07 06:12:33 +0300

    do

---
+++ src/telran/util/List.java
@@ -0,0 +1,29 @@
+package telran.util;
+
+import java.util.function.Predicate;
+
+public interface List<T> extends Collection<T> {
+	void add(int index, T obj);
+	T get(int index);
+	T set(int index, T obj);
+	T remove(int index);
+	int indexOf(Object pattern);
+	int lastIndexOf(Object pattern);
+	int indexOf(Predicate<T> predicate);
+	int lastIndexOf(Predicate<T> predicate);
+	
+	@Override
+	default public boolean remove(Object pattern) {
+		int index = indexOf(pattern);
+		boolean res = false;
+		if (index >= 0) {
+			res = true;
+			remove(index);
+		}
+		return res;
+	}
+	
+	default boolean contains(Object pattern) {
+		return indexOf(pattern) >= 0;
+	}
+}
---
+++ src/telran/util/test/ArrayListTest.java
@@ -0,0 +1,24 @@
+package telran.util.test;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import telran.util.Collection;
+import telran.util.ArrayList;
+
+class ArrayListTest extends ListTest {
+
+	@Override
+	protected Collection<Integer> getCollection(Integer[] ar1) {
+		ArrayList<Integer> arrayList = new ArrayList<>();
+		for(Integer num: ar1) {
+			arrayList.add(num);
+		}
+		return arrayList;
+	}
+	@BeforeEach
+	@Override
+	void setUp() {
+		collection = new ArrayList<>(3);
+		super.setUp();
+	}
+}
